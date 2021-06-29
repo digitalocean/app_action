6fb8ab62be03bf496d993104004e9ad5ad2339cb
param-do <85899140+param-do@users.noreply.github.com>
2021-06-29 14:05:45 -0700

    working code but error in getting static_site while parsing

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 
 	"github.com/digitalocean/godo"
 	"gopkg.in/yaml.v2"
@@ -21,14 +20,7 @@ type UpdatedRepo struct {
 
 //reads the file from fileLocation
 func readFileFrom(fileLocation string) ([]byte, error) {
-	jsonFile, err := os.Open(fileLocation)
-	if err != nil {
-		log.Fatal("Error in opening the file", err)
-		return []byte{}, err
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, err := ioutil.ReadFile(fileLocation)
 	if err != nil {
 		log.Fatal("Error in reading from file: ", err)
 		return []byte{}, err
@@ -61,6 +53,7 @@ func getAllRepo(input string, appSpec string) ([]UpdatedRepo, godo.AppSpec, erro
 	if err != nil {
 		return nil, godo.AppSpec{}, err
 	}
+	fmt.Printf("%+v\n", spec)
 	return allRepos, spec, nil
 
 }
@@ -99,32 +92,55 @@ func checkForGitAndDockerHub(allFiles []UpdatedRepo, spec *godo.AppSpec) {
 
 }
 
-func execCommand(allFiles []UpdatedRepo, appSpec godo.AppSpec) error {
+func execCommand(allFiles []UpdatedRepo, appSpec godo.AppSpec) {
 	checkForGitAndDockerHub(allFiles, &appSpec)
+	var nameMap = make(map[string]bool)
+	for val := range allFiles {
+		nameMap[allFiles[val].Name] = true
+	}
+
 	for key, _ := range allFiles {
-		cmd := exec.Command("sh", "-c", `cat _temp.yaml |yq eval '(.*[]| select(.name == "`+allFiles[key].Name+`").image.repository) |=  "`+allFiles[key].Repository+
-			`" |`+`(.*[]| select(.name == "`+allFiles[key].Name+`").image.registry_type) |= "DOCR" |(.*[]|select(.name == "`+allFiles[key].Name+`").image.tag) |=  "`+allFiles[key].Tag+`"' -| sponge _temp.yaml`)
-		_, err := cmd.Output()
-		if err != nil {
-			log.Fatal("Error in checking docr path file: ", err)
-			os.Exit(1)
+		for _, service := range appSpec.Services {
+			if service.Name != allFiles[key].Name {
+				continue
+			}
+			service.Image = &godo.ImageSourceSpec{RegistryType: "DOCR", Repository: allFiles[key].Repository, Tag: allFiles[key].Tag}
+		}
+		for _, worker := range appSpec.Workers {
+			if worker.Name != allFiles[key].Name {
+				continue
+			}
+			worker.Image = &godo.ImageSourceSpec{RegistryType: "DOCR", Repository: allFiles[key].Repository, Tag: allFiles[key].Tag}
+		}
+		for _, job := range appSpec.Jobs {
+			if job.Name != allFiles[key].Name {
+				continue
+			}
+			job.Image = &godo.ImageSourceSpec{RegistryType: "DOCR", Repository: allFiles[key].Repository, Tag: allFiles[key].Tag}
 		}
 
 	}
-	return nil
 
 }
 
 func main() {
 	//import and return json object of changed repo
-	input, appSpec, err := getAllRepo("test1", "_temp.yaml")
+	input, appSpec, err := getAllRepo("test1", "temp.yaml")
 	if err != nil {
 		fmt.Println("Error in Retrieving json data: ", err)
 		os.Exit(1)
 	}
-	err = execCommand(input, appSpec)
+	execCommand(input, appSpec)
+
+	newJson, err := json.Marshal(appSpec)
 	if err != nil {
-		log.Fatal("Error in retrieving data from")
+		log.Fatal("Error in building json spec")
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile("spec.json", newJson, 0644)
+	if err != nil {
+		log.Fatal("Error in writing json spec")
+		os.Exit(1)
 	}
 
 }
